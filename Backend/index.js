2e4ad9020aaa61b0c5ca1d392b07df07ec2c8277
abdulwahab030910 <abdulwahab030910@gmail.com>
2024-05-26 23:50:50 +0500

    route

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,26 @@
 import  express  from 'express';
 import dotenv from "dotenv"
+import mongoose from 'mongoose';
+import BookRoute from "./Route/book_route.js"
 const app = express()
 const port = process.env.PORT || 4000;
 
+
 dotenv.config()
+const URL=process.env.MONGODBURL;
+
+
+// database connection 
+
+try {
+  mongoose.connect(URL)
+  console.log("connect to mongodb")
+} catch (error) {
+  console.log("database Error",error)
+}
+
+// define routes
+app.use("/book",BookRoute)
 
 
 app.listen(port, () => {
